ed86d7923db70bc8070b5ae6cd0e7250cadfee96
jehud <jehudmetellus@outlook.com>
2026-06-30 07:39:50 -0400

    Add Precompiled Headers (PCH) to all targets to boost compile times

---
+++ sandbox/source/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef __cplusplus
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <optional>
+#include <functional>
+#include <cstdint>
+#include <stdexcept>
+
+// Third-party
+#include <flecs.h>
+#endif
