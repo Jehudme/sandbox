f9d672008377ee89771bf2955dd0e64d201f21dc
jehud <jehudmetellus@outlook.com>
2026-06-30 05:20:05 -0400

    Remove analytics module

--- modules/analytics/source/library.cpp
+++
@@ -1,7 +0,0 @@
-#include <sandbox/sdk/engine.hpp>
-
-extern "C" {
-    void initialize_analytics() {
-        // Placeholder
-    }
-}
--- test_flecs_macro.cpp
+++
@@ -1,5 +0,0 @@
-#include <flecs.h>
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-#pragma message(TOSTRING(ECS_COMPONENT_DECLARE(my_struct)))
-int main() {}
