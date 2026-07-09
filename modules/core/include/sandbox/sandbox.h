6d9b8a7f3f39fe250644b2ad74de9af82060b7ff
jehud <jehudmetellus@outlook.com>
2026-07-09 19:25:03 -0400

    Add SWIG macro to sandbox.h

--- modules/core/include/sandbox/sandbox.h
+++ modules/core/include/sandbox/sandbox.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#ifdef SWIG
+%include <sandbox/services/application_service.h>
+%include <sandbox/services/configuration_service.h>
+%include <sandbox/services/filesystem_service.h>
+%include <sandbox/services/logs_service.h>
+%include <sandbox/services/runtime_service.h>
+#else
 #include <sandbox/services/application_service.h>
 #include <sandbox/services/configuration_service.h>
 #include <sandbox/services/filesystem_service.h>
 #include <sandbox/services/logs_service.h>
 #include <sandbox/services/runtime_service.h>
+#endif
